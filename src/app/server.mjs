bb603730b5b95f92fc2bc462e9c95beae7577016
Tommaso Ricci <zorgatone@gmail.com>
2018-07-14 15:28:36 +0200

    Added express

--- src/app/server.mjs
+++ src/app/server.mjs
@@ -1,12 +1,19 @@
-import http from 'http';
+import express from 'express';
 
 import { listDatabases } from './mongo';
 
-export const createServer = () => http.createServer((req, res) => {
-  listDatabases()
-    .then((data) => res.end(data))
-    .catch((err) => {
-      console.error(err);
-      res.end('fail');
-    });
-});
+export const createServer = () => {
+  const app = express();
+
+  app.get('/', (req, res) => { res.end('Hello, World!') });
+  app.get('/admin/dbs/list', (req, res) => {
+    listDatabases()
+      .then((data) => res.end(data))
+      .catch((err) => {
+        console.error(err);
+        res.end('fail');
+      });
+  });
+
+  return app;
+};
